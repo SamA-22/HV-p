8ee6796fcffe62f9c8ecf421d9fc92ec8f89e352
Wisam Abdo <wisamabdo@hotmail.com>
2025-02-22 19:22:23 +0000

    Implementation of dynamic rendering and lazy loading for image censoring

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 const myImageUrl = chrome.runtime.getURL('hasbulla.png');
-const images = document.querySelectorAll('img');
-images.forEach(img => {
-  img.src = myImageUrl;
-  console.log(img.src)
+
+function updateImages() {
+    const images = document.querySelectorAll('img');
+    const observer = new IntersectionObserver((entires) =>
+        {
+            entires.forEach(entry => {
+                if (entry.isIntersecting){
+                    const image = entry.target;
+                    image.src = myImageUrl;
+                    observer.unobserve(image);
+                }
+            });
+        });
+    document.querySelectorAll('img').forEach(image => {
+        observer.observe(image)
+    });
+        
+}
+
+
+updateImages();
+
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.addedNodes.length) {
+      updateImages();
+    }
+  });
 });
+
+observer.observe(document.body, { childList: true, subtree: true });
