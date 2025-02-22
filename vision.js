00aec45690426846f9150d2a4bc43888e71ff5b0
X3NOCRACY <45859365+X3NOCRACY@users.noreply.github.com>
2025-02-22 16:55:59 +0000

    finding static images

---
+++ vision.js
@@ -0,0 +1,9 @@
+
+
+console.log('running');
+let images = Array.from(document.querySelectorAll("img"));
+
+for(var i = 0; i < images.length; i++) {
+    // srcList.push(images[i]);
+    console.log("image: {$i}")
+}
